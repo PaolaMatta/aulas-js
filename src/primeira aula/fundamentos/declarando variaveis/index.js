10deb5b7700ddc8eabbd08262cd9e8e1e753b44e
unknown <jefferson.sarti@vr.com.br>
2021-03-26 20:04:46 -0300

    primeira aula - declaração de variáveis

---
+++ src/primeira aula/fundamentos/declarando variaveis/index.js
@@ -0,0 +1,30 @@
+let x = 10; // estou atribuindo o valor de 10 para a variável x
+let y = "João é lindo";
+let z = true;
+
+const PI = 3.14; 
+
+/**
+ * 1) Nomes de variáveis NÃO podem começar com números e caracteres especiais. Ex.: 1nome - *peso - %quilo
+ * 2) Números podem compor o nome de uma variável. Caracteres especiais NÃO. 
+ * Ex permitidos: nome1 - cidade1 - zezinho9000 
+ * Ex proibidos: nome%do%cachorro - lixo-de-memoria
+ * 3) O ÚNICO CARACTERE QUE É PERMITIDO EM NOMES DE VARIÁVEIS É O CARACTERE _
+ * 4) Nomes de variáveis com nomes compostos ( Nome Do Cachorro ) não podem ter espaços! Nesses casos, usamos a técnica do Camel Case:
+ * Ex: NomeDoCachorro - SalarioMensal (Convenção)
+ * 5) Nomes de variáveis devem começar com a letra minúscula. (Convenção)
+ * 6) Nomes de constantes devem estar em letra maiúscula. Caso a constante possua mais de uma palavra, trocar os espaços por _
+ * Ex: DIAS_NO_MES = 30 - MEU_AMOR_PELO_JOAO_E_ETERNO = true; (Convenção)
+ * 7) NOMES DE VARIÁVEIS, FUNÇÕES, CLASSES E TUDO MAIS QUE VOCÊS ESCREVEREM, DEVEM POSSUIR SENTIDO PARA SEU CONTEXTO (Convenção)
+ */
+let a = 1.80;
+let b = 67.5;
+let c = 107;
+
+let d = (c - 32) / 1.8;
+
+let minhaAltura = 1.80;
+let meuPeso = 67.5;
+let temperaturaFahrenheight = 107;
+
+let temperaturaCelsius = (temperaturaFahrenheight - 32) / 1.8;
